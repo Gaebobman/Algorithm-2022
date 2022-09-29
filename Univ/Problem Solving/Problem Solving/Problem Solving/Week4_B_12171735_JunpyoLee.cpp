1c1d27f2938b8afe8ff96ab4950e59089d892173
Gaebobman <67403246+Gaebobman@users.noreply.github.com>
2022-09-29 13:02:28 +0900

    09.29

--- Univ/Problem Solving/Problem Solving/Problem Solving/Week4_B_12171735_JunpyoLee.cpp
+++ Univ/Problem Solving/Problem Solving/Problem Solving/Week4_B_12171735_JunpyoLee.cpp
@@ -2,7 +2,38 @@
 #include <vector>
 
 using namespace std;
-
+bool end_flag = false;
+int post_infection(const int m, const int my_self, const int parent, vector<int>& visited, vector<int>& infected, vector<vector<int>>& graph) {
+	// 1. Visited[my_self] = 1; int sum = infected[my_self] 
+	visited[my_self] = 1;
+	int sum = infected[my_self];
+	// 2. 자기 자신과 연결된 vertex들을 순회 (자식 방문)
+	for (int i = 0 ; i < graph[my_self].size(); i++) {
+		if (visited[graph[my_self][i]] == 0 && end_flag == false) {
+			sum += post_infection(m, graph[my_self][i], my_self, visited, infected, graph );
+			if (my_self == 1) {
+				if (sum == m && end_flag == false) {
+					cout << min(my_self, graph[my_self][i]) <<' '<< max(my_self, graph[my_self][i]) << '\n';
+					end_flag = true;
+					return 0;
+				}
+				 else if(end_flag == false && my_self != parent){
+					cout << -1 << '\n';
+				}
+			}
+			else if (sum == m && end_flag == false) {
+				cout << min(my_self, graph[my_self][i]) << ' ' << max(my_self, graph[my_self][i]) << '\n';
+				end_flag = true;
+				return 0;
+			}
+			else {
+				return sum;
+			}
+		}
+		
+	}
+	return sum;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -14,26 +45,25 @@ int main() {
 	cin >> T;
 
 	while (T--) {
-		cin >> n >> m;
 
-		vector<vector<int>> graph(n + 1);
-		vector<int> infected;
-		int* root;
 		int x, y;
-		memset(&root, 0, sizeof(int) * (n + 1));
-		root[1] = 1;
+		cin >> n >> m;
+		vector<int> visited(n + 1, 0);
+		vector<int> infected(n + 1, 0);
+		vector<vector<int>> graph(n + 1);
+		
 		for (int i = 0; i < n - 1; i++) {
 			cin >> x >> y;
 			graph[x].push_back(y);
 			graph[y].push_back(x);
 		}
+		
 		for (int i = 0; i < m; i++) {
 			cin >> x;
-			infected.push_back(x);
+			infected[x] = 1;
 		}
-
-	
-		free(root);
+		post_infection(m, 1, 1, visited, infected, graph);
+		end_flag = false;
 	}
 	return 0;
 }
