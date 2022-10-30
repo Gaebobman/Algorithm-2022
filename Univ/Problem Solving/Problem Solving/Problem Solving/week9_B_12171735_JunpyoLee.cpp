30e18378b4392731fdc76437fab640975bcccfa9
Gaebobman <67403246+Gaebobman@users.noreply.github.com>
2022-10-30 15:32:42 +0900

    10.30 Week9

--- Univ/Problem Solving/Problem Solving/Problem Solving/week9_B_12171735_JunpyoLee.cpp
+++ Univ/Problem Solving/Problem Solving/Problem Solving/week9_B_12171735_JunpyoLee.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -15,20 +16,19 @@ int main() {
 	while (T--) {
 		string x, y;
 		cin >> x >> y;
-		cout << levenshtein_distance(y, x) << '\n';
+		cout << levenshtein_distance(x, y) << '\n';
 	}
 	return 0;
 }
 
-int levenshtein_distance(string x, string y) {
-	int m = x.length();
-	int n = y.length();
-
+int levenshtein_distance(string a, string b) {
+	int m = a.length();
+	int n = b.length();
 	// Init Matrix (m by n size)
 	int** D = new int* [m + 1];
-	for (int i = 0; i < m; i++) {
+	for (int i = 0; i < m + 1; i++) {
 		D[i] = new int[n + 1];
-		for (int j = 0; j < n; j++) {
+		for (int j = 0; j < n + 1; j++) {
 			if (j == 0)
 				D[i][j] = i;
 			else if (i == 0)
@@ -37,15 +37,34 @@ int levenshtein_distance(string x, string y) {
 				D[i][j] = 0;
 		}
 	}
-	for (int j = 1; j <) {
 
+	// Empty String과의 거리를 세팅
+	for (int i = 1; i <= m; i++) {
+		D[i][0] = i;
+	}
+	for (int j = 1; j <= n; j++) {
+		D[0][j] = j;
 	}
 
-
-
-
-
-
+	// j -> axis Y 
+	// i -> axis X
+	for (int j = 1; j <= n; j++) {
+		for (int i = 1; i <= m; i++) {
+			if (a[i - 1] == b[j - 1]) {
+				D[i][j] = D[i - 1][j - 1];
+			}
+			else {
+				D[i][j] = min({
+				D[i - 1][j],
+				D[i][j - 1],
+				D[i - 1][j - 1]
+					}) + 1;
+				// deletion
+				// insertion
+				// substitution
+			}
+		}
+	}
 	int result = D[m][n];
 	// Delete Matrix
 	for (int i = 0; i < m + 1; i++) {
