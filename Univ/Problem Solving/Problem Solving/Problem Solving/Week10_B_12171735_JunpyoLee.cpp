8fd27fb67328033ea512aebaeb30af39a81aeefb
Gaebobman <67403246+Gaebobman@users.noreply.github.com>
2022-12-04 02:52:16 +0900

    12.04

--- Univ/Problem Solving/Problem Solving/Problem Solving/Week10_B_12171735_JunpyoLee.cpp
+++ Univ/Problem Solving/Problem Solving/Problem Solving/Week10_B_12171735_JunpyoLee.cpp
@@ -1,18 +1,25 @@
+// CORRECT
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
 struct shark {
-	int key;
 	int low;
 	int high;
 };
 
-struct compare {
+struct compare_high {
 	bool operator()(const shark& a, const shark& b) {
-		return a.high < b.high;
+		return a.high > b.high;
+	}
+};
+
+struct compare_low {
+	bool operator()(const shark& a, const shark& b) {
+		return a.low > b.low;
 	}
 };
 
@@ -24,15 +31,41 @@ int main() {
 	cin >> T;
 	while (T--) {
 		int N, M, A, B, C;
-		vector<shark> temp;
+		priority_queue<shark, vector<shark>, compare_low> temp;
 		vector<int> tank;
-		priority_queue<shark, compare> q;
+		priority_queue<shark, vector<shark>, compare_high> q;
+		int result = 0;
 		cin >> N >> M;
-		for (int i = 0; i < N; i++ ) {
+		for (int i = 0; i < N; i++) {
 			cin >> A >> B;
-			shark tmp = { i, A, B };
-			temp.push_back(tmp);
+			shark tmp = { A, B };
+			temp.push(tmp);
 		}
+		for (int i = 0; i < M; i++) {
+			cin >> C;
+			tank.push_back(C);
+		}
+		sort(tank.begin(), tank.end());
+		for (auto cur_tank = tank.begin(); cur_tank != tank.end(); cur_tank++) {
+			while (!temp.empty()) {
+				shark cur_shark = temp.top();
+				if (cur_shark.low <= *cur_tank) {
+					q.push(cur_shark);
+					temp.pop();
+				}
+				else {
+					break;
+				}
+			}
+			while (!q.empty()) {
+				shark sh = q.top(); q.pop();
+				if (sh.high >= (*cur_tank)) {
+					result++;
+					break;
+				}
+			}
+		}
+		cout << result << '\n';
 	}
 	return 0;
 }
